574a935e0bbb35bef489dbd1b55b7e749f161433
zainabraza7 <zainabraaza07@gmail.com>
2026-03-01 17:18:50 +0500

    week4lab03.java

--- week4lab03.java
+++ week4lab03.java
@@ -6,63 +6,58 @@ public class week4lab03 {
           (returns max between two values and returns max among three values
          */
 
-    // Integer addition
-    int add(int a, int b) {
-        return a + b;
-    }
 
-    // Double addition
-    double add(double a, double b) {
-        return a + b;
-    }
-
-    // Max between two integers
-    int max(int a, int b) {
-        if(a > b) {
-            return a;
-        } else {
-            return b;
+        public static void max1(int a, int b) {
+            if (a > b) {
+                System.out.println("Max: " + a);
+            }
+            else if (b > a) {
+                System.out.println("Max: " + b);
+            }
+            else {
+                System.out.println("Both numbers are equal: " + a);
+            }
         }
-    }
 
-    // Max among three integers
-    int max(int a, int b, int c) {
-        int m = a;
-        if (b > m)
-            m = b;
-        if (c > m)
-            m = c;
-        return m;
-    }
+        public static void max2(double a, double b, double c) {
+            if (a > b && a > c) {
+                System.out.println("Max: " + a);
+            }
+            else if (b > a && b > c) {
+                System.out.println("Max: " + b);
+            }
+            else if (c > a && c > b) {
+                System.out.println("Max: " + c);
+            }
+            else {
+                System.out.println("All numbers are equal");
+            }
+        }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        week4lab03 c = new week4lab03();  // Object of the same class
+        public static void main(String[] args) {
 
-        System.out.println("INTEGER OPERATIONS");
-        System.out.print("Enter first integer: ");
-        int num1 = sc.nextInt();
+            Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter second integer: ");
-        int num2 = sc.nextInt();
+            System.out.print("Enter first integer: ");
+            int x = sc.nextInt();
 
-        System.out.println("Sum = " + c.add(num1, num2));
-        System.out.println("Max of two = " + c.max(num1, num2));
+            System.out.print("Enter second integer: ");
+            int y = sc.nextInt();
 
-        System.out.print("Enter third integer: ");
-        int num3 = sc.nextInt();
+            max1(x, y);
 
-        System.out.println("Max of three = " + c.max(num1, num2, num3));
+            System.out.print("Enter first double: ");
+            double a = sc.nextDouble();
 
-        System.out.println(" DOUBLE OPERATIONS");
-        System.out.print("Enter first double: ");
-        double d1 = sc.nextDouble();
+            System.out.print("Enter second double: ");
+            double b = sc.nextDouble();
 
-        System.out.print("Enter second double: ");
-        double d2 = sc.nextDouble();
+            System.out.print("Enter third double: ");
+            double c = sc.nextDouble();
 
-        System.out.println("Sum = " + c.add(d1, d2));
+            max2(a, b, c);
 
-        sc.close();
-    }
+            sc.close();
+        }
+
 }
